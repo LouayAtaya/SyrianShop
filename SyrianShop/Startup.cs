3708b613c44e370d43c89379f7fa17542242199f
Mohamad louay Ataya <louay.ataya@gmail.com>
2021-06-22 09:32:26 +0300

    Add support Swagger

--- SyrianShop/Startup.cs
+++ SyrianShop/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using SyrianShop.dataContexts;
 using SyrianShop.errorHandling;
 using SyrianShop.models;
@@ -62,6 +63,8 @@ namespace SyrianShop
                     return new BadRequestObjectResult(errorResponse);
                 }
             );
+
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo() { Title = "Syrian Shop API", Version = "v1" }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -77,6 +80,10 @@ namespace SyrianShop
             app.UseCors();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c=> 
+                { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Syrian Shop API"); });
 
             app.UseEndpoints(endpoints =>
             {
