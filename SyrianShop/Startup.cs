aa0816c74b57173b4e6ec53b18948dac3208fd08
Mohamad louay Ataya <louay.ataya@gmail.com>
2021-06-22 10:14:58 +0300

    Cleaning up the Startup class

--- SyrianShop/Startup.cs
+++ SyrianShop/Startup.cs
@@ -9,12 +9,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using SyrianShop.configuration;
 using SyrianShop.dataContexts;
-using SyrianShop.errorHandling;
-using SyrianShop.models;
-using SyrianShop.repositories;
+
 
 namespace SyrianShop
 {
@@ -35,36 +33,10 @@ namespace SyrianShop
             services.AddDbContext<SyrianShopContext>(o =>
                 o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddScoped<ProductRepository, ProductRepository>();
+            //an extension for IServiceCollection
+            services.AddApplicationServices();
 
-            services.AddControllers().AddNewtonsoftJson(options =>
-                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
-
-            services.AddCors(c =>
-                {
-                    c.AddPolicy("CorsPolicy", policy =>
-                     {
-                         policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
-                     }); 
-                });
-
-            services.Configure<ApiBehaviorOptions>(options =>
-                options.InvalidModelStateResponseFactory=actionContext =>
-                {
-                    var errors = actionContext.ModelState
-                        .Where(e => e.Value.Errors.Count > 0)
-                        .SelectMany(x => x.Value.Errors)
-                        .Select(x => x.ErrorMessage).ToArray();
-
-                    var errorResponse = new ApiValidationErrorResponse()
-                    {
-                        Errors = errors
-                    };
-                    return new BadRequestObjectResult(errorResponse);
-                }
-            );
-
-            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo() { Title = "Syrian Shop API", Version = "v1" }));
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -81,9 +53,8 @@ namespace SyrianShop
 
             app.UseAuthorization();
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c=> 
-                { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Syrian Shop API"); });
+            //extension foe IApplicationBuilder to support swagger 
+            app.UseSwaggerDocumentation();
 
             app.UseEndpoints(endpoints =>
             {
---
+++ SyrianShop/configuration/ApplicationServicesExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using SyrianShop.dataContexts;
+using SyrianShop.errorHandling;
+using SyrianShop.repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SyrianShop.configuration
+{
+    public static class ApplicationServicesExtensions
+    {
+
+        //Extension method for IserviceCollection
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //repositories
+            services.AddScoped<ProductRepository, ProductRepository>();
+
+            //json cycle
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            //enable cors
+            services.AddCors(c =>
+            {
+                c.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+                });
+            });
+
+
+            //model validation custom message
+            services.Configure<ApiBehaviorOptions>(options =>
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState
+                        .Where(e => e.Value.Errors.Count > 0)
+                        .SelectMany(x => x.Value.Errors)
+                        .Select(x => x.ErrorMessage).ToArray();
+
+                    var errorResponse = new ApiValidationErrorResponse()
+                    {
+                        Errors = errors
+                    };
+                    return new BadRequestObjectResult(errorResponse);
+                }
+            );
+
+            return services;
+        }
+    }
+}
